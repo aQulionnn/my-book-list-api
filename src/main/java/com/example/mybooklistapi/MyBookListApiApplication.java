93eddb4ea5e49c41dfc3758bde2222cbc7e9319d
bekasker <2004bek.asker@mail.ru>
2026-01-22 22:38:38 +0500

    Initial commit

---
+++ src/main/java/com/example/mybooklistapi/MyBookListApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mybooklistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBookListApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBookListApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mybooklistapi/MyBookListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mybooklistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBookListApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
